3d347a44e1463009c6fda620ac45d35aac8736a0
migbyte-0 <152094060+migbyte-0@users.noreply.github.com>
2025-01-25 13:59:03 +0300

    Update cubit boilerplate code

--- lua/archflow/init.lua
+++ lua/archflow/init.lua
@@ -14,77 +14,6 @@ local function create_file(path, content)
   end
 end
 
-local function generate_mvc_with_bloc_files(feature_path, feature_name)
-  -- Bloc files
-  local bloc_file = feature_path .. "/controller/" .. feature_name .. "_bloc.dart"
-  local bloc_content = [[
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part ']] .. feature_name .. [[_event.dart';
-part ']] .. feature_name .. [[_state.dart';
-
-class ]] .. feature_name:gsub("^%l", string.upper) .. [[Bloc extends Bloc<]] .. feature_name:gsub("^%l", string.upper) .. [[Event, ]] .. feature_name:gsub("^%l", string.upper) .. [[State> {
-  ]] .. feature_name:gsub("^%l", string.upper) .. [[Bloc() : super(const ]] .. feature_name:gsub("^%l", string.upper) .. [[State()) {
-    on<ExampleEvent>(_mapExampleEventToState);
-  }
-
-  void _mapExampleEventToState(ExampleEvent event, Emitter<]] .. feature_name:gsub("^%l", string.upper) .. [[State> emit) {
-    emit(state.copyWith(message: "Handled ExampleEvent"));
-  }
-}
-]]
-  create_file(bloc_file, bloc_content)
-
-  -- Event file
-  local event_file = feature_path .. "/controller/" .. feature_name .. "_event.dart"
-  local event_content = [[
-part of ']] .. feature_name .. [[_bloc.dart';
-
-abstract class ]] .. feature_name:gsub("^%l", string.upper) .. [[Event extends Equatable {
-  const ]] .. feature_name:gsub("^%l", string.upper) .. [[Event();
-
-  @override
-  List<Object?> get props => [];
-}
-
-class ExampleEvent extends ]] .. feature_name:gsub("^%l", string.upper) .. [[Event {}
-]]
-  create_file(event_file, event_content)
-
-  -- State file
-  local state_file = feature_path .. "/controller/" .. feature_name .. "_state.dart"
-  local state_content = [[
-part of ']] .. feature_name .. [[_bloc.dart';
-
-enum ]] .. feature_name:gsub("^%l", string.upper) .. [[Status { initial, loading, success, error }
-
-class ]] .. feature_name:gsub("^%l", string.upper) .. [[State extends Equatable {
-  const ]] .. feature_name:gsub("^%l", string.upper) .. [[State({
-    this.status = ]] .. feature_name:gsub("^%l", string.upper) .. [[Status.initial,
-    this.message = '',
-  });
-
-  final ]] .. feature_name:gsub("^%l", string.upper) .. [[Status status;
-  final String message;
-
-  ]] .. feature_name:gsub("^%l", string.upper) .. [[State copyWith({
-    ]] .. feature_name:gsub("^%l", string.upper) .. [[Status? status,
-    String? message,
-  }) {
-    return ]] .. feature_name:gsub("^%l", string.upper) .. [[State(
-      status: status ?? this.status,
-      message: message ?? this.message,
-    );
-  }
-
-  @override
-  List<Object?> get props => [status, message];
-}
-]]
-  create_file(state_file, state_content)
-end
-
 local function generate_mvc_with_cubit_files(feature_path, feature_name)
   -- Cubit files
   local cubit_file = feature_path .. "/controller/" .. feature_name .. "_cubit.dart"
@@ -135,6 +64,38 @@ class ]] .. feature_name:gsub("^%l", string.upper) .. [[State extends Equatable 
 }
 ]]
   create_file(state_file, state_content)
+
+  -- Model file
+  local model_file = feature_path .. "/model/" .. feature_name .. "_model.dart"
+  local model_content = [[
+class ]] .. feature_name:gsub("^%l", string.upper) .. [[Model {
+  // Add your model properties and methods here
+}
+]]
+  create_file(model_file, model_content)
+
+  -- View file
+  local view_file = feature_path .. "/view/" .. feature_name .. "_view.dart"
+  local view_content = [[
+import 'package:flutter/material.dart';
+
+class ]] .. feature_name:gsub("^%l", string.upper) .. [[View extends StatelessWidget {
+  const ]] .. feature_name:gsub("^%l", string.upper) .. [[View({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(']] .. feature_name:gsub("^%l", string.upper) .. [[ View'),
+      ),
+      body: Center(
+        child: Text('Welcome to ]] .. feature_name:gsub("^%l", string.upper) .. [[ View'),
+      ),
+    );
+  }
+}
+]]
+  create_file(view_file, view_content)
 end
 
 local function generate_architecture_structure(base_path, feature_name, architecture, state_management)
@@ -147,9 +108,7 @@ local function generate_architecture_structure(base_path, feature_name, architec
       create_directory(feature_path .. "/" .. dir)
     end
 
-    if state_management == "bloc" then
-      generate_mvc_with_bloc_files(feature_path, feature_name)
-    elseif state_management == "cubit" then
+    if state_management == "cubit" then
       generate_mvc_with_cubit_files(feature_path, feature_name)
     end
   end
